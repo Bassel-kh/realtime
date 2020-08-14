b0a1c938d9a8b80deaef627690ae9f80726caddd
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-14 13:44:49 +0300

    #4     #m002 ارسال واستقبال اشعارات الزمن الحقيقي ب لارافل و سوكت سرفر- laravel notification puhser (part 1) add Models (comment , posts)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
-<?php
+ <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+ Route::post('comment', 'HomeController@saveComment')->name('comment.save');
